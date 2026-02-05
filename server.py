a4aa3a56e3e3d45b5f9d47fd3d5644d28e04049d
Boris Chazalet <bchazalet@companywatch.net>
2026-02-05 16:00:45 +0100

    Add the simplest python webserver

---
+++ server.py
@@ -0,0 +1,13 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b'<h1>Hello, World!</h1>')
+
+if __name__ == '__main__':
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHandler)
+    print('Server running on http://0.0.0.0:8000')
+    server.serve_forever()
